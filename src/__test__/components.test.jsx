0a63b110689c4da9041342180b76a09528d2daa9
MR. ANDOH FRANCIS <andohfrancis9187@gmail.com>
2023-08-30 10:28:29 +0000

    added test for components

--- src/__test__/components.test.jsx
+++ src/__test__/components.test.jsx
@@ -1,10 +1,33 @@
+import { render } from '@testing-library/react';
 import Home from '../components/Home';
-import { render, screen } from '@testing-library/react';
+import Navbar from '../components/Navbar';
+import Calculator from '../components/Calculator';
+import Footer from '../components/Footer';
+import Quote from '../components/Quote';
 
 describe('Testing Calculator Components', () => {
-    describe('Testing Home Component', () => {
-        
-    }
-    )
+  test('should render home component', () => {
+    const home = render(<Home />);
+    expect(home).toMatchSnapshot();
+  });
 
-})
+  test('should render Navbar', () => {
+    const navbar = render(<Navbar />);
+    expect(navbar).toMatchSnapshot();
+  });
+
+  test('should render Calculator', () => {
+    const calculator = render(<Calculator />);
+    expect(calculator).toMatchSnapshot();
+  });
+
+  test('should render Footer', () => {
+    const footer = render(<Footer />);
+    expect(footer).toMatchSnapshot();
+  });
+
+  test('should render Quote', () => {
+    const quote = render(<Quote />);
+    expect(quote).toMatchSnapshot();
+  });
+});
--- src/__test__/operate.test.jsx
+++ src/__test__/operate.test.jsx
@@ -1,29 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import operate  from '../logic/operate';
+import operate from '../logic/operate';
 
-describe("Testing operate function", () => {
-    test("testing addition", () => {
-        expect(operate(1, 2, "+")).toBe("3");
-    });
-    test("testing subtraction", () => {
-        expect(operate(2, 1, "-")).toBe("1");
-    });
-    test("testing multiplication", () => {
-        expect(operate(2, 2, "x")).toBe("4");
-    });
-    test("testing division", () => {
-        expect(operate(4, 2, "÷")).toBe("2");
-    });
-    test("testing modulo", () => {
-        expect(operate(4, 2, "%")).toBe("0");
-    });
-    test("testing unknown operation", () => {
-        expect(() => operate(4, 2, "a")).toThrow("Unknown operation 'a'");
-    });
-    test("testing division by zero", () => {
-        expect(operate(4, 0, "÷")).toBe("Can't divide by 0.");
-    });
-    test("testing modulo by zero", () => {
-        expect(operate(4, 0, "%")).toBe("Can't find modulo as can't divide by 0.");
-    });
-})
+describe('Testing operate function', () => {
+  test('testing addition', () => {
+    expect(operate(1, 2, '+')).toBe('3');
+  });
+  test('testing subtraction', () => {
+    expect(operate(2, 1, '-')).toBe('1');
+  });
+  test('testing multiplication', () => {
+    expect(operate(2, 2, 'x')).toBe('4');
+  });
+  test('testing division', () => {
+    expect(operate(4, 2, '÷')).toBe('2');
+  });
+  test('testing modulo', () => {
+    expect(operate(4, 2, '%')).toBe('0');
+  });
+  test('testing unknown operation', () => {
+    expect(() => operate(4, 2, 'a')).toThrow("Unknown operation 'a'");
+  });
+  test('testing division by zero', () => {
+    expect(operate(4, 0, '÷')).toBe("Can't divide by 0.");
+  });
+  test('testing modulo by zero', () => {
+    expect(operate(4, 0, '%')).toBe("Can't find modulo as can't divide by 0.");
+  });
+});
