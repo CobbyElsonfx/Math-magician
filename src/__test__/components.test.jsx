d14a8ade40e19178ba71fb1802c4fb82543ad386
MR. ANDOH FRANCIS <andohfrancis9187@gmail.com>
2023-08-30 04:58:47 +0000

    created testing files

---
+++ src/__test__/components.test.jsx
@@ -0,0 +1,10 @@
+import Home from '../components/Home';
+import { render, screen } from '@testing-library/react';
+
+describe('Testing Calculator Components', () => {
+    describe('Testing Home Component', () => {
+        
+    }
+    )
+
+})
